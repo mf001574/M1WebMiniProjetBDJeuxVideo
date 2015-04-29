cdc8b6b944a4b01e2e22e76cba5691fe733ff2de
Florian Massa <mf001574>
2015-04-30 01:10:04 +0200

    version fonctionnelle

---
+++ PlayInHTML5Projet/src/java/gestionnaires/GestionnaireUtilisateur.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionnaires;
+
+import javax.ejb.Stateless;
+import java.util.Collection;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import modeles.Utilisateur;
+
+/**
+ *
+ * @author Florian Massa
+ */
+@Stateless
+public class GestionnaireUtilisateur {
+    
+    @PersistenceContext
+    private EntityManager em; 
+    
+    private int nbUtilisateurs;
+    
+    public GestionnaireUtilisateur(){
+        this.nbUtilisateurs = 0;
+        System.out.println("Je suis dans le contructeur");
+    }
+
+    public EntityManager getEm() {
+        return em;
+    } 
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+    
+    public int getNbUtilisateurs() {
+        return nbUtilisateurs;
+    }
+
+    public void setNbUtilisateurs(int nbUtilisateurs) {
+        this.nbUtilisateurs = nbUtilisateurs;
+    }
+  
+    
+    
+    public void creer1000UtilisateursDeTest(){
+        for(int i = 1 ; i<=1000;i++){
+            this.creerUtilisateur("Utilisateur"+this.nbUtilisateurs ,"test",0);
+        }
+       
+    }
+    public Utilisateur creerUtilisateur(String login, String mdp,int type) {
+        Utilisateur u = new Utilisateur(login,mdp,type);
+        this.nbUtilisateurs++;
+        em.persist(u);
+        return u;
+    }
+    
+    public Collection<Utilisateur> getAllUsers(){
+        Query q = em.createQuery("select u from Utilisateur u order by u.id");
+        //q.setFirstResult(this.indiceDepart);
+       // q.setMaxResults(10);
+        return q.getResultList();
+    }
+}
---
+++ PlayInHTML5Projet/web/vueAccueil.jsp
@@ -0,0 +1,10 @@
+<%-- 
+    Document   : index.jsp
+    Created on : 9 avr. 2015, 16:23:45
+    Author     : florian
+--%>
+
+<jsp:include page="template.jsp">
+	<jsp:param name="content" value="contenuVueAccueil"/>
+	<jsp:param name="title" value="Page d'accueil"/>
+</jsp:include>
